005834de4be54977e5a8ff8bfeaff74fd188bc65
dennis932 <dennisbro932@gmail.com>
2023-07-19 00:36:12 -0700

    task-0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main -entry point
+ * Description: test the positive_or_negative function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
